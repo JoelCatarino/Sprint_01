435f546dac7a8092ec29a2eec42f05e3e8d6d561
Joel Catarino <joelscatarino@outlook.com>
2023-05-27 12:36:13 -0300

    final-commit
    ultimo commit do challenger em Desktop frist

--- main.js
+++ main.js
@@ -1,9 +1,57 @@
-import './style.css'
+const input = document.querySelector(".input");
+const output = document.querySelector(".output");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-  
-  </div>
-`
+function btnEncrypt() {
+  const encryptedText = encrypt(input.value);
+  output.value = encryptedText;
+  input.value = "";
+}
 
-setupCounter(document.querySelector('#counter'))
+function btnDecrypt() {
+  const decryptedText = decrypt(output.value);
+  input.value = decryptedText;
+  output.value = "";
+}
+
+function encrypt(EncryptedString) {
+  let base = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  EncryptedString = EncryptedString.toLowerCase();
+
+  for (let i = 0; i < base.length; i++) {
+    if (EncryptedString.includes(base[i][0])) {
+      EncryptedString = EncryptedString.replaceAll(base[i][0], base[i][1]);
+    }
+  }
+  return EncryptedString;
+}
+
+function decrypt(encryptedString) {
+  let dbase = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+
+  for (let i = 0; i < dbase.length; i++) {
+    if (encryptedString.includes(dbase[i][1])) {
+      encryptedString = encryptedString.replaceAll(dbase[i][1], dbase[i][0]);
+    }
+  }
+
+  return encryptedString;
+}
+
+function copyText() {
+  const input = document.querySelector(".input");
+  const output = document.querySelector(".output");
+
+  navigator.clipboard.writeText(input.value + "\n" + output.value);
+}
